8ee9a07514a0bb0711f36a1d904f746a3bfb078f
krishnadev7 <krishnadevv12@gmail.com>
2023-08-17 22:08:27 +0530

    completed rbs2

--- rotated_BS/Rbs2.java
+++ rotated_BS/Rbs2.java
@@ -1,23 +1,52 @@
 public class Rbs2 {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2,3};
-        System.out.println(pivot(arr));
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2, 3 };
+        System.out.println(findPivot(arr));
     }
-    static int pivot(int[] arr){
+
+    static int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if (pivot == -1) {
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+        if(nums[pivot] == target){
+            return pivot;
+        }
+        if(target >= nums[0]){
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    static int findPivot(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-        while(start <= end){
+        while (start <= end) {
             int mid = start + (end - start) / 2;
-            if(mid < end && arr[mid] > arr[mid + 1]){
+            if (mid < end && arr[mid] > arr[mid + 1]) {
                 return mid;
             }
-            if(start < mid && arr[mid] < arr[mid - 1]){
+            if (start < mid && arr[mid] < arr[mid - 1]) {
                 return mid - 1;
             }
-            if(arr[mid] <= arr[start]){
+            if (arr[mid] <= arr[start]) {
                 end = mid - 1;
-            }else{
+            } else {
                 start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
             }
         }
         return -1;
